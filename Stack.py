ad6389b71d673079b92328893330bbe12f3f75ec
Eduard Scheske <eduard.scheske@googlemail.com>
2014-06-23 10:10:43 +0200

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -4,72 +4,52 @@
 class Stack:
 	"""
 	"""
-	class Node:
-		"""
-		"""
-		def __init__(self, data, next):
-			self.data = data
-			self.next = next
 	
-	def __init__(self ): # data=None):
-		self.TopNode = None
-		self.Size = 0
+	def __init__(self ):
+		self.Nodes = []
 	
 	def isEmpty(self):
-		return (self.Size == 0)
+		return len(self) == 0
 	
-	@property
-	def size(self):
-		return self.Size
+	def __len__(self):
+		return len(self.Nodes)
 	
 	def push(self, data):
-		self.TopNode = self.Node( data, self.TopNode )
-		self.Size += 1
+		self.Nodes.append( data )
 	
 	def pop(self):
-		if self.isEmpty():
-			raise Exception('Stack is Empty')
-		else:
-			returnNode = self.TopNode
-			self.TopNode = self.TopNode.next
-			self.Size -= 1
-			return returnNode.data
+		assert not self.isEmpty(), "Stack is Empty"
+		return self.Nodes.pop()
 	
 	def peek(self):
-		if self.isEmpty():
-			raise Exception('Stack is Empty')
-		else:
-			return self.TopNode.data
+		assert not self.isEmpty(), "Stack is Empty"
+		return self.Nodes[-1]
 
 if __name__ == "__main__":
-	debug = False
-	if debug == True:
-		TestData = [ "1. Element", "2. Element", 42, 4.2, "Letztes Element" ]
-		
-		print( "Testcase for Stack:" )
-		
-		print( "\nTest push() from Stack class" )
-		WorkStack = Stack()
-		for el in TestData:
-			print( "push to Stack:", el )
-			WorkStack.push( el )
-		
-		print( "\nTest Size of Stack class" )
-		print( "Stack size", WorkStack.size )
-		
-		print( "\nTest peek() from Stack class" )
-		data = WorkStack.peek()
-		print( "Peeked data from Stack:", data )
-		
-		print( "\nTest pop() from Stack class" )
-		while not WorkStack.isEmpty():
-			data = WorkStack.pop()
-			print( "Stack data:", data )
-		
-		print( "\nTest pop() from empty Stack class. Should raise an Exception" )
-		try:
-			WorkStack.pop()
-		except Exception as E:
-			print( "Stack raised an Exception" )
-		
+	TestData = [ "1. Element", "2. Element", 42, 4.2, "Letztes Element" ]
 	
+	print( "Testcase for Stack:" )
+	
+	print( "\nTest push() from Stack class" )
+	WorkStack = Stack()
+	for el in TestData:
+		print( "push to Stack:", el )
+		WorkStack.push( el )
+	
+	print( "\nTest Size of Stack class" )
+	print( "Stack size", len(WorkStack) )
+	
+	print( "\nTest peek() from Stack class" )
+	data = WorkStack.peek()
+	print( "Peeked data from Stack:", data )
+	
+	print( "\nTest pop() from Stack class" )
+	while not WorkStack.isEmpty():
+		data = WorkStack.pop()
+		print( "Stack data:", data )
+	
+	print( "\nTest pop() from empty Stack class. Should raise an Exception" )
+	try:
+		WorkStack.pop()
+	except Exception as E:
+		print( "Stack raised an Exception" )
